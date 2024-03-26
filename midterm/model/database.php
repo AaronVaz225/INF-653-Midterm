440f492b4f53c8bcd0e47fcbdb3c18e9c5018dc4
AaronVaz225 <129011088+AaronVaz225@users.noreply.github.com>
2024-03-25 19:32:26 -0500

    Update database.php

--- midterm/model/database.php
+++ midterm/model/database.php
@@ -1,10 +1,11 @@
 <?php 
 
-$dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-$username = 'root';
+$dsn = 'mysql:host=ysp9sse09kl0tzxj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=y6hcj8o8vp0qclaz';
+$username = 'bix13ahz4s8w7s6p';
+$pass = 'rg7crj9m1w67jfau';
 
 try{
-    $db = new PDO($dsn, $username);
+    $db = new PDO($dsn, $username, $pass);
 
 } catch(PDOException $e){
 $error = "Database Error: ";
